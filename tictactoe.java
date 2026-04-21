1544269b1efd455296b5ef33634fe695af769d1b
AtharVMauryaOne <harshmaurya045@gmail.com>
2026-04-21 14:14:19 +0530

    Update tictactoe.java

--- tictactoe.java
+++ tictactoe.java
@@ -1,1 +1,14 @@
+System.out.print("Enter position (1-9): ");
+int pos = sc.nextInt();
 
+// Validate input
+if(pos < 1 || pos > 9) {
+    System.out.println("Invalid position!");
+}
+
+// Convert to row & column
+int row = (pos - 1) / 3;
+int col = (pos - 1) % 3;
+
+// Place move
+board[row][col] = player;
