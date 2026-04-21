8e4a52756ed4fa88de6f8ed46a1f2efb38da8f15
AtharVMauryaOne <harshmaurya045@gmail.com>
2026-04-21 14:11:58 +0530

    Update tictactoe.java

--- tictactoe.java
+++ tictactoe.java
@@ -1,1 +1,11 @@
+char[][] board = new char[3][3];
 
+// Initialize board
+for(int i = 0; i < 3; i++){
+    for(int j = 0; j < 3; j++){
+        board[i][j] = '-';
+    }
+}
+
+// Display board
+printBoard(board);
